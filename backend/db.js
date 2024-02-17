76f709b5acff4c60b2229ca7a44e6a942f26b618
lifeashaze <34096693+lifeashaze@users.noreply.github.com>
2024-02-17 22:52:54 +0530

    initialise db and route

---
+++ backend/db.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+mongoose.connect("mongodb+srv://admin:admin@cluster0.mzzxlpo.mongodb.net/")
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
+
+const User = mognoose.model('User',userSchema);
+
+module.exports = {
+    User
+};
