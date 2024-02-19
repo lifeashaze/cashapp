c9032c8660ddd12caf7b197d8838ce6b1dfeff05
lifeashaze <34096693+lifeashaze@users.noreply.github.com>
2024-02-19 17:27:45 +0530

    Added Components and connected signup page to backend

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import { Appbar } from "../components/AppBar"
+import { Balance } from "../components/Balance"
+import { Users } from "../components/Users"
 
 export const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+    return <div>
+        <Appbar />
+        <div className="m-8">
+            <Balance value={"10,000"} />
+            <Users />
+        </div>
+    </div>
 }
-
